73675275a2ef4554c163629b9d93e4554e7773af
Shawn <shawn.rushefsky@gmail.com>
2024-09-25 15:52:21 -0400

    flux stuff

--- src/flux1-schnell-fp8-comfyui/workflows/flux/img2img.ts
+++ src/flux1-schnell-fp8-comfyui/workflows/flux/img2img.ts
@@ -12,6 +12,8 @@ type ComfyNode = z.infer<typeof ComfyNodeSchema>;
 interface Workflow {
   RequestSchema: z.ZodObject<any, any>;
   generateWorkflow: (input: any) => Record<string, ComfyNode>;
+  description?: string;
+  summary?: string;
 }
 
 let checkpoint: any = config.models.checkpoints.enum.optional();
@@ -210,6 +212,8 @@ function generateWorkflow(input: InputType): Record<string, ComfyNode> {
 const workflow: Workflow = {
   RequestSchema,
   generateWorkflow,
+  summary: "Image to Image",
+  description: "Generate an image from another image and a text prompt",
 };
 
 export default workflow;
