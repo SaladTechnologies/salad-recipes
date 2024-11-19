9bbaaf1eb020a62442ac780889c01f803ff449b3
Shawn <shawn.rushefsky@gmail.com>
2024-11-19 10:41:12 -0500

    automation updates

--- benchmark/process-results.js
+++ benchmark/process-results.js
@@ -22,7 +22,7 @@ const outputResultsFile =
 const summaryFile = rawResultsFile.replace(/\.jsonl?$/, "-summary.json");
 const nodeCountCSV = rawResultsFile.replace(/\.jsonl?$/, "-node-count.csv");
 const testConfigFile = rawResultsFile.replace(/\.jsonl?$/, "-test-config.json");
-const priceFile = process.argv[4] || 'benchmark/prices.json';
+const priceFile = process.argv[4] || "benchmark/prices.json";
 
 const vCPUPrice = 0.004;
 const memGBPrice = 0.001;
@@ -64,7 +64,7 @@ function makePriceMap(prices) {
   return priceMap;
 }
 
-const getHourlyPrice = (priceMap, gpu, priority, vCPUs, memGB, maxNodes) => {
+function getHourlyPrice(priceMap, gpu, priority, vCPUs, memGB, maxNodes) {
   if (!priceMap[gpu]) {
     throw new Error(`Unknown GPU: ${gpu}`);
   }
@@ -77,23 +77,10 @@ const getHourlyPrice = (priceMap, gpu, priority, vCPUs, memGB, maxNodes) => {
       memGB * memGBPrice) *
     maxNodes;
   return hourlyPrice;
-};
+}
 
-async function processResults() {
-  const rawData = await fs.readFile(rawResultsFile, "utf-8");
-  const rawLines = rawData.split("\n");
-
-  const nodeCounts = await fs.readFile(nodeCountCSV, "utf-8");
-  const nodeLines = nodeCounts.split("\n");
-
-  const testConfigContents = await fs.readFile(testConfigFile, "utf-8");
-  const testConfig = JSON.parse(testConfigContents);
-
-  const priceData = await fs.readFile(priceFile, "utf-8");
-  const prices = JSON.parse(priceData);
-  const priceMap = makePriceMap(prices);
-
-  const allResults = rawLines
+function condenseRawData(k6Lines, nodeCountLines) {
+  const allResults = k6Lines
     .map((line) => {
       if (!line) {
         return null;
@@ -102,20 +89,22 @@ async function processResults() {
     })
     .filter(Boolean);
 
-  const nodeResults = nodeLines.map((line) => {
-    if (!line) {
-      return null;
-    }
-    const [time, count] = line.split(",");
-    return {
-      type: "Point",
-      metric: "node_count",
-      data: {
-        time: new Date(time * 1000).toISOString(),
-        value: parseInt(count),
-      },
-    };
-  }).filter(Boolean);
+  const nodeResults = nodeCountLines
+    .map((line) => {
+      if (!line) {
+        return null;
+      }
+      const [time, count] = line.split(",");
+      return {
+        type: "Point",
+        metric: "node_count",
+        data: {
+          time: new Date(time * 1000).toISOString(),
+          value: parseInt(count),
+        },
+      };
+    })
+    .filter(Boolean);
 
   allResults.push(...nodeResults);
 
@@ -136,39 +125,39 @@ async function processResults() {
     timeFromStart: new Date(result.data.time) - firstTime,
   }));
 
-  const smallResults = {
-    resources: testConfig.container.resources,
-    metric: results.map((r) => r.metric),
-    timeFromStart: results.map((r) => r.timeFromStart),
-    value: results.map((r) => r.value),
-  };
+  return results;
+}
 
-  // Now we generate a summary that can be passed to an llm in order to generate
-  // a blog post
+function getPercentiles(data, percentiles) {
+  const sortedData = data.toSorted((a, b) => a.value - b.value);
+  return percentiles.map((p) => {
+    const index = Math.floor(data.length * p);
+    if (index === data.length) {
+      return sortedData[index - 1].value;
+    }
+    return sortedData[index].value;
+  });
+}
+
+function summarizeResults(results, prices, testConfig) {
   const durations = results.filter((r) => r.metric === "http_req_duration");
   const failedRequests = results.filter(
     (r) => r.metric === "http_req_failed" && r.value > 0
   );
   const vus = results.filter((r) => r.metric === "vus");
 
-  const lengthOfBenchmarkSeconds =
-    (Math.max(...smallResults.timeFromStart) -
-      Math.min(...smallResults.timeFromStart)) /
-    1000;
+  const lengthOfBenchmarkSeconds = Math.max(
+    ...results.map((r) => r.timeFromStart)
+  ) / 1000;
   const numRequests = durations.length;
   const numFailedRequests = failedRequests.length;
   const successRate = (numRequests - numFailedRequests) / numRequests;
 
-  const minDurationSeconds =
-    durations.reduce((acc, val) => Math.min(acc, val.value), Infinity) / 1000;
-  const maxDurationSeconds =
-    durations.reduce((acc, val) => Math.max(acc, val.value), 0) / 1000;
-  const meanDurationSeconds =
-    durations.reduce((acc, val) => acc + val.value, 0) / (numRequests * 1000);
-  const percentile90DurationSeconds =
-    durations.map((r) => r.value).sort((a, b) => a - b)[
-      Math.floor(numRequests * 0.9)
-    ] / 1000;
+  const [minDurationSeconds, maxDurationSeconds, medianDurationSeconds, p90DurationSeconds] = getPercentiles(durations, [0, 1, 0.5, 0.9]).map((v) => v / 1000);
+  const meanDurationSeconds = durations.reduce(
+    (acc, val) => acc + val.value,
+    0
+  ) / durations.length / 1000;
 
   const throughputPeriod = 60 * 1000;
   const throughput = durations.map((d, i) => {
@@ -187,16 +176,18 @@ async function processResults() {
     "value"
   );
 
-  const avgThroughput = numRequests / lengthOfBenchmarkSeconds;
-  const maxThroughput = Math.max(
-    ...rollingThroughput.map((r) => r.value),
-  );
-  const timeOfMaxThroughputSeconds = rollingThroughput.find(
-    (r) => r.value === maxThroughput,
-  ).timeFromStart / 1000;
-  
+  const meanThroughput = numRequests / lengthOfBenchmarkSeconds;
+  const [p10Throughput, medianThroughput, maxThroughput] = getPercentiles(rollingThroughput, [0.1, 0.5, 1]);
+  const timeOfMaxThroughputSeconds =
+    rollingThroughput.find((r) => r.value === maxThroughput).timeFromStart / 1000;
+
   // Find the number of VUs at the time of max throughput. This should be done by seeking vus until time is greater than timeOfMaxThroughput, then taking the value before that
-  const vusAtMaxThroughput = vus[vus.findIndex((r) => r.timeFromStart / 1000 > timeOfMaxThroughputSeconds) - 1].value;
+  const vusAtMaxThroughput =
+    vus[
+      vus.findIndex(
+        (r) => r.timeFromStart / 1000 > timeOfMaxThroughputSeconds
+      ) - 1
+    ].value;
 
   const minVUs = Math.min(...vus.map((r) => r.value));
   const maxVUs = Math.max(...vus.map((r) => r.value));
@@ -205,11 +196,12 @@ async function processResults() {
     .filter((r, i) => vus[i - 1]?.value !== r.value)
     .map((r) => ({ ...r, timeFromStart: r.timeFromStart / 1000 }));
 
-  const minnodeCount = Math.min(...nodeResults.map((r) => r.data.value));
-  const maxnodeCount = Math.max(...nodeResults.map((r) => r.data.value));
+  const nodeResults = results.filter((r) => r.metric === "node_count");
+  const minNodeCount = Math.min(...nodeResults.map((r) => r.value));
+  const maxNodeCount = Math.max(...nodeResults.map((r) => r.value));
   const nodeCountFrequency = nodeResults.reduce((acc, val) => {
-    const count = acc[val.data.value] || 0;
-    acc[val.data.value] = count + 1;
+    const count = acc[val.value] || 0;
+    acc[val.value] = count + 1;
     return acc;
   }, {});
   const nodeCountMode = Object.entries(nodeCountFrequency).reduce(
@@ -221,44 +213,112 @@ async function processResults() {
     },
     { value: null, count: 0 }
   );
+  nodeCountMode.value = parseInt(nodeCountMode.value);
+  const meanNodeCount = nodeResults.reduce(
+    (acc, val) => acc + val.value,
+    0
+  ) / nodeResults.length;
 
-  const gpuName = getGPUNameByID(prices, testConfig.container.resources.gpu_classes[0]).toLowerCase();
-  const hourlyCost = getHourlyPrice(priceMap,  gpuName, "batch", testConfig.container.resources.cpu, testConfig.container.resources.memory / 1024, maxnodeCount);
-  const bestCostPerRequest = hourlyCost / (maxThroughput * 60 * 60);
-  const bestRequestsPerDollar = 1 / bestCostPerRequest;
+  const priceMap = makePriceMap(prices);
+  const gpuName = getGPUNameByID(
+    prices,
+    testConfig.container.resources.gpu_classes[0]
+  ).toLowerCase();
+  const hourlyCost = getHourlyPrice(
+    priceMap,
+    gpuName,
+    "batch",
+    testConfig.container.resources.cpu,
+    testConfig.container.resources.memory / 1024,
+    maxNodeCount
+  );
+  const [ medianCostPerRequest, meanCostPerRequest, minCostPerRequest ] = [medianThroughput, meanThroughput, maxThroughput].map((t) => hourlyCost / (t * 60 * 60));
 
-  const summary = {
+  return {
     recipeName: path.basename(path.dirname(path.dirname(rawResultsFile))),
-    resources: {...testConfig.container.resources, gpu_classes: [gpuName]},
+    resources: { ...testConfig.container.resources, gpu_classes: [gpuName] },
     lengthOfBenchmarkSeconds,
     numRequests,
     numFailedRequests,
     successRate,
-    minDurationSeconds,
-    maxDurationSeconds,
-    meanDurationSeconds,
-    percentile90DurationSeconds,
-    avgThroughput,
-    maxThroughput,
-    timeOfMaxThroughputSeconds,
-    vusAtMaxThroughput,
-    minnodeCount,
-    maxnodeCount,
-    nodeCountMode: parseInt(nodeCountMode.value),
-    minVUs,
-    maxVUs,
-    requestsPerDollar: bestRequestsPerDollar,
-    hourlyCost,
-    bestCostPerRequest,
+    duration: {
+      min: minDurationSeconds,
+      median: medianDurationSeconds,
+      mean: meanDurationSeconds,
+      p90: p90DurationSeconds,
+      max: maxDurationSeconds,
+    },
+    throughput: {
+      p10: p10Throughput,
+      median: medianThroughput,
+      mean: meanThroughput,
+      max: maxThroughput,
+      timeAtMax: timeOfMaxThroughputSeconds,
+      vusAtMax: vusAtMaxThroughput,
+    },
+    nodeCounts: {
+      numSamples: nodeResults.length,
+      min: minNodeCount,
+      max: maxNodeCount,
+      mode: nodeCountMode,
+      mean: meanNodeCount,
+    },
+    vus: {
+      min: minVUs,
+      max: maxVUs,
+    },
+    cost: {
+      perHour: hourlyCost,
+      perRequest: {
+        median: medianCostPerRequest,
+        mean: meanCostPerRequest,
+        min: minCostPerRequest,
+      },
+      requestsPerDollar: {
+        median: 1 / medianCostPerRequest,
+        mean: 1 / meanCostPerRequest,
+        max: 1 / minCostPerRequest,
+      }
+    },
     vuTimeline,
   };
+}
 
-  console.log(`Processed ${results.length} results`);
-  await fs.writeFile(summaryFile, JSON.stringify(summary, null, 2));
+async function processResults() {
+  const rawData = await fs.readFile(rawResultsFile, "utf-8");
+  const rawLines = rawData.split("\n");
+
+  const nodeCounts = await fs.readFile(nodeCountCSV, "utf-8");
+  const nodeLines = nodeCounts.split("\n");
+
+  const results = condenseRawData(rawLines, nodeLines);
+
+  const testConfigContents = await fs.readFile(testConfigFile, "utf-8");
+  const testConfig = JSON.parse(testConfigContents);
+
+  const priceData = await fs.readFile(priceFile, "utf-8");
+  const prices = JSON.parse(priceData);
+
+  const smallResults = {
+    resources: testConfig.container.resources,
+    metric: results.map((r) => r.metric),
+    timeFromStart: results.map((r) => r.timeFromStart),
+    value: results.map((r) => r.value),
+  };
+
+  console.log(`Writing data to ${outputResultsFile}`);
   await fs.writeFile(outputResultsFile, JSON.stringify(smallResults, null, 2));
+
+  // Now we generate a summary that can be passed to an llm in order to generate
+  // a blog post
+
+  const summary = summarizeResults(results, prices, testConfig);
+  console.log(`Writing summary to ${summaryFile}`);
+  await fs.writeFile(summaryFile, JSON.stringify(summary, null, 2));
+  
 }
 
 processResults().catch((e) => {
   console.error(e);
   process.exit(1);
-})
+});
