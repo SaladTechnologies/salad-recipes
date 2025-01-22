5a998a438ced11106a10deeca55ab54c0dbdc31e
Shawn <shawn.rushefsky@gmail.com>
2025-01-22 10:59:49 -0500

    remove outdated example that is not a recipe

--- src/example-image/readiness-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "This test will disable the ready status for this container instance for approximately 2 minutes. After which, it should resume being ready."
-rm status
-echo "Readiness disabled."
-sleep 120
-echo "200" > status
-echo "Readiness is back to active."
