a9fd3b0c21ea58119753e322698e29cce05b65cd
Shawn <shawn.rushefsky@gmail.com>
2025-04-30 10:40:12 -0400

    refine hello world

--- src/hello-world/app.py
+++ src/hello-world/app.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI
+import os
 
+salad_machine_id = os.getenv("SALAD_MACHINE_ID", "localhost")
 
 app = FastAPI()
 
 
 @app.get("/hello")
 async def hello_world():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World!", "salad_machine_id": salad_machine_id}
 
 
 @app.get("/started")
-async def start_probe():
+async def startup_probe():
     return {"message": "Started!"}
 
 
 @app.get("/ready")
-async def ready_probe():
+async def readiness_probe():
     return {"message": "Ready!"}
 
 
 @app.get("/live")
-async def live_probe():
+async def liveness_probe():
     return {"message": "Live!"}
