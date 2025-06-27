437bf34c084775d26b8b3bcf8c966a3c24fed308
gorkii <m.gorki14@gmail.com>
2025-06-27 07:52:59 -0400

    yolo update

--- recipes/yolo/inference/fast.py
+++ recipes/yolo/inference/fast.py
@@ -17,7 +17,29 @@ import requests
 import os
 
 
-async def process_video_annotated(cap):
+# Initialize FastAPI app
+app = FastAPI()
+
+model = os.getenv("MODEL", "yolov8m.pt")
+
+if torch.cuda.is_available():
+        # Find which gpu is the best 
+        best_gpu = None
+        max_memory = 0
+        for i in range(torch.cuda.device_count()):
+            props = torch.cuda.get_device_properties(i)
+            if props.total_memory > max_memory:
+                max_memory = props.total_memory
+                best_gpu = i
+        
+        if best_gpu is not None:
+            torch.cuda.set_device(best_gpu)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Load the YOLOv8 model
+model = YOLO(model)
+
+async def process_video_annotated(cap, conf, iou, imgsz, track, tracker, device=device):
     width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS) or 30
@@ -32,7 +54,10 @@ async def process_video_annotated(cap):
         success, frame = cap.read()
         if not success:
             break
-        results = model(frame)
+        if track:
+            results = model.track(frame, conf=conf, iou=iou, imgsz=imgsz, tracker=tracker, device=device)
+        else:
+            results = model.predict(frame, conf=conf, iou=iou, imgsz=imgsz, device=device)
         annotated = results[0].plot()
         out.write(annotated)
 
@@ -40,7 +65,7 @@ async def process_video_annotated(cap):
     return out_path
 
 
-async def process_video(cap):
+async def process_video(cap, conf, iou, imgsz, track, tracker, device=device):
     all_detections = []
     frame_count = 0
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -51,7 +76,10 @@ async def process_video(cap):
         if not success:
             break
         timestamp = frame_count / fps
-        results = model(frame)
+        if track:
+            results = model.track(frame, conf=conf, iou=iou, imgsz=imgsz, tracker=tracker, device=device)
+        else:
+            results = model.predict(frame, conf=conf, iou=iou, imgsz=imgsz, device=device)
         detections = json.loads(results[0].tojson())
         for detection in detections:
             detection['timestamp'] = timestamp
@@ -61,35 +89,17 @@ async def process_video(cap):
     return all_detections
 
 
-# Initialize FastAPI app
-model = os.getenv("MODEL", "yolov8m.pt")
-app = FastAPI()
 
-if torch.cuda.is_available():
-        # Find which gpu is the best 
-        best_gpu = None
-        max_memory = 0
-        for i in range(torch.cuda.device_count()):
-            props = torch.cuda.get_device_properties(i)
-            if props.total_memory > max_memory:
-                max_memory = props.total_memory
-                best_gpu = i
-        
-        if best_gpu is not None:
-            torch.cuda.set_device(best_gpu)
-
-
-# Load the YOLOv8 model
-model = YOLO(model)
 
 @app.post("/process_file")
 async def process_file(
     file: UploadFile = File(...),
     annotated: bool = Query(False),
-    conf: float = Query(0.25, ge=0, le=1),
-    iou: float = Query(0.7, ge=0, le=1),
-    imgsz: Optional[int] = Query(None),
-
+    conf: float = Query(0.25),
+    iou: float = Query(0.7),
+    imgsz: int = Query(640),
+    track: bool = Query(False),
+    tracker: Optional[str] = Query("bytetrack.yaml")
 ):
     contents = await file.read()
     # Try to read as an image
@@ -97,7 +107,7 @@ async def process_file(
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     if img is not None:
         # Process as image
-        results = model(img)
+        results = model.predict(img, conf=conf, iou=iou, imgsz=imgsz, device=device)
         if annotated:
             annotated_img = results[0].plot()
             _, buffer = cv2.imencode('.jpg', annotated_img)
@@ -107,14 +117,13 @@ async def process_file(
             detections = json.loads(results[0].tojson())
             # Return JSON with labels and detections
             return detections
-    else:
-        # Assume it's a video file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp:
-            temp.write(contents)
-            temp_filename = temp.name
+    # Assume it's a video file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp:
+        temp.write(contents)
+        temp_filename = temp.name
         cap = cv2.VideoCapture(temp_filename)
         if annotated:
-            output_path = await process_video_annotated(cap)
+            output_path = await process_video_annotated(cap, conf, iou, imgsz, track, tracker, device=device)
             cap.release()
             os.remove(temp_filename)
 
@@ -125,7 +134,7 @@ async def process_file(
 
             return StreamingResponse(iterfile(), media_type="video/mp4")
         else:
-            detections = await process_video(cap)
+            detections = await process_video(cap, conf, iou, imgsz, track, tracker, device=device)
             cap.release()
             os.remove(temp_filename)
             return detections
@@ -137,33 +146,56 @@ class URLRequest(BaseModel):
 
 @app.post("/process_url")
 async def process_url(
-    url_request: URLRequest
+    url_request: URLRequest,
+    annotated: bool = Query(False),
+    conf: float = Query(0.25),
+    iou: float = Query(0.7),
+    imgsz: int = Query(640),
+    track: bool = Query(False),
+    tracker: Optional[str] = Query("bytetrack.yaml")
 ):
     url = url_request.url
     try:
         if "youtube.com" in url or "youtu.be" in url:
             cap = cap_from_youtube(url, "720p")
-            detections = await process_video(cap)
-            cap.release()
-            # os.remove(temp_video_file.name)
-            return detections
+            if annotated:
+                output_path = await process_video_annotated(cap, conf, iou, imgsz, track, tracker, device=device)
+                cap.release()
+                def iterfile():
+                    with open(output_path, "rb") as f:
+                        yield from f
+                    os.remove(output_path)
+
+                return StreamingResponse(iterfile(), media_type="video/mp4")
+            else:
+                detections = await process_video(cap, conf, iou, imgsz, track, tracker, device=device)
+                cap.release()
+                return detections
         else:
-                # Check if the URL is an image
-                response = requests.get(url, stream=True)
-                content_type = response.headers.get('Content-Type', '')
-                if 'image' in content_type:
-                    # Download the image
-                    img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
-                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    if img is not None:
-                        # Process as image
-                        results = model(img)
-                        detections = json.loads(results[0].tojson())
-                        return detections
+            # Check if the URL is an image
+            response = requests.get(url, stream=True)
+            content_type = response.headers.get('Content-Type', '')
+            if 'image' in content_type:
+                # Download the image
+                img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                if img is not None:
+                    # Process as image
+                    results = model.predict(img, conf=conf, iou=iou, imgsz=imgsz, device=device)
+                    if annotated:
+                        annotated_img = results[0].plot()
+                        _, buffer = cv2.imencode('.jpg', annotated_img)
+                        return StreamingResponse(io.BytesIO(buffer), media_type="image/jpeg")
                     else:
-                        return JSONResponse(content={"error": "Could not decode the image"}, status_code=400)
-                else:
-                    return JSONResponse(content={"error": "Invalid URL: Not an image or YouTube video"}, status_code=400)
+                        # Collect labels and detections
+                        detections = json.loads(results[0].tojson())
+                        # Return JSON with labels and detections
+                        return detections
+
+            else:
+                return JSONResponse(content={"error": "Could not decode the image"}, status_code=400)
+            # else:
+            #     return JSONResponse(content={"error": "Invalid URL: Not an image or YouTube video"}, status_code=400)
     except Exception as e:
         return JSONResponse(content={"error": f"An error occurred: {str(e)}"}, status_code=500)
 
